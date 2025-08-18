6aa55ba8b63b92db6c639e19ee473c8dcc6babef
andrei-barysenka <andrei.barysenka@gmail.com>
2025-08-18 15:41:22 +0200

    updated UserDao.java

--- src/main/java/cloudgene/mapred/database/UserDao.java
+++ src/main/java/cloudgene/mapred/database/UserDao.java
@@ -4,9 +4,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Vector;
+import java.util.Set;
+import java.util.HashSet;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import cloudgene.mapred.core.User;
 import cloudgene.mapred.database.util.Database;
@@ -16,7 +18,7 @@ import cloudgene.mapred.util.PublicUser;
 
 public class UserDao extends JdbcDataAccessObject {
 
-	private static final Logger log = LoggerFactory.getLogger(UserDao.class);
+	private static final Log log = LogFactory.getLog(UserDao.class);
 
 	public UserDao(Database database) {
 		super(database);
@@ -24,13 +26,12 @@ public class UserDao extends JdbcDataAccessObject {
 
 	public boolean insert(User user) {
 		StringBuilder sql = new StringBuilder();
-		sql.append(
-				"insert into `user` (username, password, full_name, aws_key, aws_secret_key, save_keys, export_to_s3, s3_bucket, mail, role, export_input_to_s3, activation_code, active, api_token, last_login, locked_until, login_attempts, api_token_expires_on) ");
-		sql.append("values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+		// max_running has default value=2
+		sql.append("insert into `user` (username, password, full_name, aws_key, aws_secret_key, save_keys, export_to_s3, s3_bucket, mail, role, export_input_to_s3, activation_code, active, api_token, last_login, locked_until, login_attempts, institute_email, institute_name, institute_address1, institute_address2, institute_city, institute_postcode, institute_country, accepted_t_c, accepted_eu_eea, api_token_expires_on)");
+		sql.append("values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
 
 		try {
-
-			Object[] params = new Object[18];
+			Object[] params = new Object[27];
 			params[0] = user.getUsername().toLowerCase();
 			params[1] = user.getPassword();
 			params[2] = user.getFullName();
@@ -48,7 +49,18 @@ public class UserDao extends JdbcDataAccessObject {
 			params[14] = user.getLastLogin();
 			params[15] = user.getLockedUntil();
 			params[16] = user.getLoginAttempts();
-			params[17] = user.getApiTokenExpiresOn();
+
+			params[17] = user.getInstituteEmail();
+			params[18] = user.getInstituteName();
+			params[19] = user.getInstituteAddress1();
+			params[20] = user.getInstituteAddress2();
+			params[21] = user.getInstituteCity();
+			params[22] = user.getInstitutePostCode();
+			params[23] = user.getInstituteCountry();
+
+			params[24] = user.getAcceptedTandC();
+			params[25] = user.getAcceptedCountry();
+			params[26] = user.getApiTokenExpiresOn();
 
 			int id = insert(sql.toString(), params);
 
@@ -64,6 +76,7 @@ public class UserDao extends JdbcDataAccessObject {
 		return true;
 	}
 
+    // max_running is not affected here
 	public boolean update(User user) {
 		StringBuilder sql = new StringBuilder();
 		sql.append(
@@ -71,7 +84,7 @@ public class UserDao extends JdbcDataAccessObject {
 		sql.append("where id = ?");
 
 		try {
-
+			// TODO: Update so that institute info can also be updated 
 			Object[] params = new Object[19];
 			params[0] = user.getUsername().toLowerCase();
 			params[1] = user.getPassword();
@@ -104,6 +117,24 @@ public class UserDao extends JdbcDataAccessObject {
 
 		return true;
 	}
+
+    // sets max_running for all users
+    public boolean setMaxRunningJobs(int n){
+	StringBuilder sql = new StringBuilder();
+	sql.append("update `user` set max_running = ?");
+
+	try {
+	    Object[] params = new Object[1];
+	    params[0] = n;
+	    update(sql.toString(), params);
+	    log.debug("successfully set max_running to "+n);
+	} catch (SQLException e) {
+	    log.error("setting max_running to "+n+" failed", e);
+	    return false;
+	}
+
+	return true;
+    }
 
 	public User findByUsername(String user) {
 
@@ -229,6 +260,7 @@ public class UserDao extends JdbcDataAccessObject {
 		}
 	}
 
+
 	@SuppressWarnings("unchecked")
 	public List<User> findByQuery(String query) {
 
@@ -243,7 +275,7 @@ public class UserDao extends JdbcDataAccessObject {
 		params[0] = "%" + query + "%";
 		params[1] = params[0];
 		params[2] = params[0];
-
+		
 		List<User> result = new Vector<User>();
 
 		try {
@@ -288,8 +320,20 @@ public class UserDao extends JdbcDataAccessObject {
 		return result;
 	}
 
-	public boolean delete(User user) {
+    /* all unique roles from 'user' table, over all users */
+	@SuppressWarnings("unchecked")
+	public Set<String> findAllRoles() {
+	    List<User> users=findAll();
+	    Set<String> roles=new HashSet<String>();
+	    for (User user:users){
+		for (String role:user.getRoles()){
+		    roles.add(role.toLowerCase());
+		}
+	    }
+	    return roles;
+	}
 
+	public boolean delete(User user) {
 		// update all older jobs
 		User publicUser = PublicUser.getUser(database);
 
@@ -326,6 +370,7 @@ public class UserDao extends JdbcDataAccessObject {
 		public User mapRow(ResultSet rs, int row) throws SQLException {
 			User user = new User();
 			user.setId(rs.getInt("user.id"));
+			user.setMaxRunningJobs(rs.getInt("user.max_running"));
 			user.setUsername(rs.getString("user.username"));
 			user.setPassword(rs.getString("user.password"));
 			user.setFullName(rs.getString("user.full_name"));
@@ -338,10 +383,10 @@ public class UserDao extends JdbcDataAccessObject {
 			user.setActivationCode(rs.getString("user.activation_code"));
 			user.setActive(rs.getBoolean("user.active"));
 			user.setApiToken(rs.getString("user.api_token"));
+			user.setApiTokenExpiresOn(rs.getTimestamp("user.api_token_expires_on"));
 			user.setLastLogin(rs.getTimestamp("user.last_login"));
 			user.setLockedUntil(rs.getTimestamp("user.locked_until"));
 			user.setLoginAttempts(rs.getInt("user.login_attempts"));
-			user.setApiTokenExpiresOn(rs.getTimestamp("user.api_token_expires_on"));
 			return user;
 		}
 
