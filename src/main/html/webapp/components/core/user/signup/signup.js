44ae9417e8084232b9185da4dad1e2696a91191f
andrei-barysenka <andrei.barysenka@gmail.com>
2025-08-18 15:00:58 +0200

    update signup.js

--- src/main/html/webapp/components/core/user/signup/signup.js
+++ src/main/html/webapp/components/core/user/signup/signup.js
@@ -2,58 +2,87 @@ import Control from 'can-control';
 import $ from 'jquery';
 
 import User from 'models/user';
+import Country from 'models/country-signup';
 
 import template from './signup.stache';
-
+import "./signup.css";
 
 export default Control.extend({
+  "init": function (element, options) {
+    var params = {};
+    Country.findAll(
+      params,
+      function (countries) {
+        $(element).html(template({
+          countries: countries
+        }));
+        $(element).fadeIn();
+      },
+      function (response) {
+        new ErrorPage(element, response);
+      });
 
-  "init": function(element, options) {
-    this.emailRequired = options.appState.attr('emailRequired');
-    $(element).hide();
-    $(element).html(template({
-      emailRequired: options.appState.attr('emailRequired'),
-      userEmailDescription: options.appState.attr('userEmailDescription'),
-      userWithoutEmailDescription: options.appState.attr('userWithoutEmailDescription')
-    }));
-    $(element).fadeIn();
-  },
+    // $(element).hide();
+    // $(element).html(template());
+    // $(element).fadeIn();
 
+    $(document).on("click", "#tos-view-btn", function () {
+      $("#terms-and-conditions").removeClass("hidden");
+    });
 
-  "#anonymous1 click" : function(){
-    this.updateEmailControl();
-  },
+    $(document).on("click", "#dpa-view-btn", function () {
+      $("#data-processing-agreement").removeClass("hidden");
+    });
 
-  "#anonymous2 click" : function(){
-    this.updateEmailControl();
-  },
+    $(document).on("click", ".close-btn", function () {
+      $("#terms-and-conditions").addClass("hidden");
+      $("#data-processing-agreement").addClass("hidden");
+    });
 
-  "updateEmailControl": function() {
-      if (!this.emailRequired){
-        var anonymousControl = $(this.element).find("[name='anonymous']:checked");
-        var anonymous = (anonymousControl.val() == "1");
-        var mail = $(this.element).find("[name='mail']");
-        if (anonymous){
-          mail.attr('disabled','disabled');
-        } else {
-          mail.removeAttr('disabled');
+    // We use the MutationObserver to detect that the .content under #terms-and-conditions has been added to the DOM
+    var observer = new MutationObserver(function (mutations) {
+      mutations.forEach(function (mutation) {
+        if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
+          var contentElement = document.querySelector("#terms-and-conditions .tac-background .content");
+          if (contentElement) {
+            contentElement.addEventListener("scroll", function () {
+              var scrollTop = this.scrollTop;
+              var tcHeight = this.clientHeight;
+              var scrolled = Math.ceil(scrollTop + tcHeight) + 5;
+              var scrollHeight = this.scrollHeight;
+              if (scrolled >= scrollHeight) {
+                document.querySelector('#accept-terms-and-conditions').disabled = false;
+              }
+            });
+            observer.disconnect();
+          }
+
+          var contentElement = document.querySelector("#data-processing-agreement .tac-background .content");
+          if (contentElement) {
+            contentElement.addEventListener("scroll", function () {
+              var scrollTop = this.scrollTop;
+              var tcHeight = this.clientHeight;
+              var scrolled = Math.ceil(scrollTop + tcHeight) + 5;
+              var scrollHeight = this.scrollHeight;
+              // console.log(scrolled);
+              // console.log(scrollHeight);
+              if (scrolled >= scrollHeight) {
+                document.querySelector('#accept-eu').disabled = false;
+              }
+            });
+            observer.disconnect();
+          }
         }
-      }
-   },
+      });
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
+  },
 
-  'submit': function(element, event) {
+  'submit': function (element, event) {
     event.preventDefault();
 
     var that = this;
     var user = new User();
-
-    // anonymous radiobutton
-    var anonymous = false;
-
-    if (!this.emailRequired){
-      var anonymousControl = $(element).find("[name='anonymous']:checked");
-      anonymous = (anonymousControl.val() == "1");
-    }
 
     // username
     var username = $(element).find("[name='username']");
@@ -67,12 +96,8 @@ export default Control.extend({
 
     // mail
     var mail = $(element).find("[name='mail']");
-    if (!anonymous){
-      var mailError = user.checkMail(mail.val());
-      this.updateControl(mail, mailError);
-    } else {
-      this.updateControl(mail, undefined);
-    }
+    var mailError = user.checkMail(mail.val());
+    this.updateControl(mail, mailError);
 
     // password
     var newPassword = $(element).find("[name='new-password']");
@@ -80,26 +105,62 @@ export default Control.extend({
     var passwordError = user.checkPassword(newPassword.val(), confirmNewPassword.val());
     this.updateControl(newPassword, passwordError);
 
-    if (usernameError || fullnameError || mailError || passwordError) {
+    // institute email
+    var instituteEmail = $(element).find("[name='institute-mail']")
+    var instituteEmailError = (instituteEmail.val() !== "" ? undefined : 'Must input the email of your institute supervisor/legal-representative')
+    this.updateControl(instituteEmail, instituteEmailError);
+
+    // institute name
+    var instituteName = $(element).find("[name='institute-name']")
+    var instituteNameError = (instituteName.val() !== "" ? undefined : 'Must input your institute name')
+    this.updateControl(instituteName, instituteNameError);
+
+    // institute address
+    var instituteAddress1 = $(element).find("[name='institute-address1']")
+    var instituteAddress1Error = (instituteAddress1.val() !== "" ? undefined : 'Must input your institute address')
+    this.updateControl(instituteAddress1, instituteAddress1Error);
+
+    // institute city
+    var instituteCity = $(element).find("[name='institute-city']")
+    var instituteCityError = (instituteCity.val() !== "" ? undefined : 'Must input your institute city')
+    this.updateControl(instituteCity, instituteCityError);
+
+    // institute postal code
+    var institutePostCode = $(element).find("[name='institute-postcode']")
+    var institutePostCodeError = (institutePostCode.val() !== "" ? undefined : 'Must input your institute postal code')
+    this.updateControl(institutePostCode, institutePostCodeError);
+
+    // institute country
+    var instituteCountry = $(element).find("[name='institute-country']")
+    var instituteCountryError = (instituteCountry.val() !== "" ? undefined : 'Must select a country for your institute')
+    this.updateControl(instituteCountry, instituteCountryError);
+
+    // terms & conditions
+    var termsAndConditions = $(element).find("[name='accept-terms-and-conditions']") // document.querySelector('#accept-terms-and-conditions').checked;
+    var termsAndConditionsError = (termsAndConditions[0].checked ? undefined : 'Must acknowledge that you are fully authorized to accept these Terms of Service on behalf of your institute')
+    this.updateControl(termsAndConditions, termsAndConditionsError);
+
+    // EU-/EAA-country
+    var termsAndConditionsCountry = $(element).find("[name='accept-eu']")
+    var termsAndConditionsCountryError = (termsAndConditionsCountry[0].checked ? undefined : 'Must agree to accept the Terms of Service including the Data Processing Agreement')
+    this.updateControl(termsAndConditionsCountry, termsAndConditionsCountryError);
+
+
+    if (usernameError || fullnameError || mailError || passwordError || instituteEmailError || instituteNameError || instituteAddress1Error || instituteCityError || institutePostCodeError || instituteCountryError || termsAndConditionsError || termsAndConditionsCountryError) {
       return false;
     }
 
     $('#save').button('loading');
 
     $.ajax({
-      url: "api/v2/users/register",
+      url: "/api/v2/users/register",
       type: "POST",
       data: $(element).find("#signon-form").serialize(),
       dataType: 'json',
-      success: function(data) {
+      success: function (data) {
         if (data.success == true) {
           // shows success
-          var message = "";
-          if (!anonymous){
-            message = "Well done!</b> An email including the activation code has been sent to your address."
-          } else {
-            message = "<b>Well done!</b> Your account is now active. <a href=\"/\">Login now</a>."
-          }
+          var message = "Well done!</b> An email including the activation code has been sent to your address.";
 
           $('#signon-form').hide();
           $('#success-message').html(message);
@@ -112,7 +173,7 @@ export default Control.extend({
 
         }
       },
-      error: function(message) {
+      error: function (message) {
         alert('failure: ' + message);
         $('#save').button('reset');
       }
@@ -120,7 +181,7 @@ export default Control.extend({
 
   },
 
-  updateControl: function(control, error) {
+  updateControl: function (control, error) {
     if (error) {
       control.removeClass('is-valid');
       control.addClass('is-invalid');
