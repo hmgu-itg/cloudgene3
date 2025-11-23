b58f58d3a049d83ae0301b2a82af7a9d4f1013f0
andrei-barysenka <andrei.barysenka@gmail.com>
2025-11-23 15:10:51 +0100

    add JS code to deal with DPA and TOS

--- src/main/html/webapp/components/core/user/signup/signup.js
+++ src/main/html/webapp/components/core/user/signup/signup.js
@@ -23,56 +23,57 @@ export default Control.extend({
         new ErrorPage(element, response);
       });
 	
-    // $(document).on("click", "#tos-view-btn", function () {
-    //   $("#terms-and-conditions").removeClass("hidden");
-    // });
+    $(document).on("click", "#tos-view-btn", function () {
+      $("#terms-and-conditions").removeClass("hidden");
+    });
 
-    // $(document).on("click", "#dpa-view-btn", function () {
-    //   $("#data-processing-agreement").removeClass("hidden");
-    // });
+    $(document).on("click", "#dpa-view-btn", function () {
+      $("#data-processing-agreement").removeClass("hidden");
+    });
 
-    // $(document).on("click", ".close-btn", function () {
-    //   $("#terms-and-conditions").addClass("hidden");
-    //   $("#data-processing-agreement").addClass("hidden");
-    // });
+    $(document).on("click", ".close-btn", function () {
+      $("#terms-and-conditions").addClass("hidden");
+      $("#data-processing-agreement").addClass("hidden");
+    });
 
-    // // We use the MutationObserver to detect that the .content under #terms-and-conditions has been added to the DOM
-    // var observer = new MutationObserver(function (mutations) {
-    //   mutations.forEach(function (mutation) {
-    //     if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
-    //       var contentElement = document.querySelector("#terms-and-conditions .tac-background .content");
-    //       if (contentElement) {
-    //         contentElement.addEventListener("scroll", function () {
-    //           var scrollTop = this.scrollTop;
-    //           var tcHeight = this.clientHeight;
-    //           var scrolled = Math.ceil(scrollTop + tcHeight) + 5;
-    //           var scrollHeight = this.scrollHeight;
-    //           if (scrolled >= scrollHeight) {
-    //             document.querySelector('#accept-terms-and-conditions').disabled = false;
-    //           }
-    //         });
-    //         observer.disconnect();
-    //       }
+    // We use the MutationObserver to detect that the .content under #terms-and-conditions has been added to the DOM
+    var observer = new MutationObserver(function (mutations) {
+      mutations.forEach(function (mutation) {
+        if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
+          var contentElement = document.querySelector("#terms-and-conditions .tac-background .content");
+          if (contentElement) {
+            contentElement.addEventListener("scroll", function () {
+              var scrollTop = this.scrollTop;
+              var tcHeight = this.clientHeight;
+              var scrolled = Math.ceil(scrollTop + tcHeight) + 5;
+              var scrollHeight = this.scrollHeight;
+              if (scrolled >= scrollHeight) {
+                document.querySelector('#accept-terms-and-conditions').disabled = false;
+              }
+            });
+            observer.disconnect();
+          }
 
-    //       var contentElement = document.querySelector("#data-processing-agreement .tac-background .content");
-    //       if (contentElement) {
-    //         contentElement.addEventListener("scroll", function () {
-    //           var scrollTop = this.scrollTop;
-    //           var tcHeight = this.clientHeight;
-    //           var scrolled = Math.ceil(scrollTop + tcHeight) + 5;
-    //           var scrollHeight = this.scrollHeight;
-    //           // console.log(scrolled);
-    //           // console.log(scrollHeight);
-    //           if (scrolled >= scrollHeight) {
-    //             document.querySelector('#accept-eu').disabled = false;
-    //           }
-    //         });
-    //         observer.disconnect();
-    //       }
-    //     }
-    //   });
-    // });
-	// observer.observe(document.body, { childList: true, subtree: true });
+          var contentElement = document.querySelector("#data-processing-agreement .tac-background .content");
+          if (contentElement) {
+            contentElement.addEventListener("scroll", function () {
+              var scrollTop = this.scrollTop;
+              var tcHeight = this.clientHeight;
+              var scrolled = Math.ceil(scrollTop + tcHeight) + 5;
+              var scrollHeight = this.scrollHeight;
+              // console.log(scrolled);
+              // console.log(scrollHeight);
+              if (scrolled >= scrollHeight) {
+                document.querySelector('#accept-eu').disabled = false;
+              }
+            });
+            observer.disconnect();
+          }
+        }
+      });
+    });
+	observer.observe(document.body, { childList: true, subtree: true });
+	
 	this.emailRequired=true;
     //this.emailRequired = options.appState.attr('emailRequired');
     $(element).hide();
