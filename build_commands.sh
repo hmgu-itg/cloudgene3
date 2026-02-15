10eac41ae7f2a5de17d105a0ef5e3ce6a3789200
andrei-barysenka <andrei.barysenka@gmail.com>
2026-02-15 18:43:52 +0100

    update build commands

--- build_commands.sh
+++ build_commands.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/bash
 
+# /mnt/storage/cloudgene3
+dest_dir=$1
+
 mvn -Dmaven.test.skip=true package
 
 cd ~/repos/cloudgene3/target/cloudgene-3.1.3
 chmod 750 cloudgene
-rm -rf /local/cloudgene3/webapp ; cp -r webapp /local/cloudgene3/
+rm -rf "${dest_dir}"/webapp ; cp -r webapp "${dest_dir}"
 # rm -rf /local/cloudgene3/cloudgene_exec/sample ; cp -r sample /local/cloudgene3/
 # rm -rf /local/cloudgene3/cloudgene_exec/lib ; cp -r lib /local/cloudgene3/
-rm /local/cloudgene3/cloudgene ; cp cloudgene /local/cloudgene3/
-rm /local/cloudgene3/cloudgene.jar ; cp cloudgene.jar /local/cloudgene3/
+rm -f "${dest_dir}"/cloudgene ; cp cloudgene "${dest_dir}"
+rm -f "${dest_dir}"/cloudgene.jar ; cp cloudgene.jar "${dest_dir}"
