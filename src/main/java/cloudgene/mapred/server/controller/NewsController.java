929209a3107edf2e7275a692322cb13d8a809c3c
andrei-barysenka <andrei.barysenka@gmail.com>
2026-03-07 14:07:04 +0100

    add addNews ro controller

--- src/main/java/cloudgene/mapred/server/controller/NewsController.java
+++ src/main/java/cloudgene/mapred/server/controller/NewsController.java
@@ -2,6 +2,8 @@ package cloudgene.mapred.server.controller;
 
 import cloudgene.mapred.server.services.NewsService;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.context.annotation.Parameter;
+import io.micronaut.context.annotation.Consumes;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.security.rules.SecurityRule;
 import io.micronaut.security.annotation.Secured;
@@ -12,6 +14,14 @@ public class NewsController {
 	@Inject
 	protected NewsService newsService;
 
+	@Post("/api/v2/users/addnews")
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Secured(SecurityRule.IS_ANONYMOUS)
+	//TODO: only allow admin to add/delete news
+	//@Secured(User.ROLE_ADMIN)
+	public boolean addNews( @Parameter("text") String  text) {
+		return newsService.addNews(text);
+	}
 	@Get("/api/v2/users/news")
 	@Secured(SecurityRule.IS_ANONYMOUS)
 	//@Secured(User.ROLE_ADMIN)
