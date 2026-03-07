4d1ff4ad069b77cc78280e561c75da786ebb63be
andrei-barysenka <andrei.barysenka@gmail.com>
2026-03-07 15:04:41 +0100

    add deletenews endpoint

--- src/main/java/cloudgene/mapred/server/controller/NewsController.java
+++ src/main/java/cloudgene/mapred/server/controller/NewsController.java
@@ -20,21 +20,30 @@ public class NewsController {
 	@Inject
 	protected NewsService newsService;
 
+	@Post("/api/v2/users/deletenews/{ID}")
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Secured(SecurityRule.IS_ANONYMOUS)
+	//TODO: only allow admin to add/delete news
+	//@Secured(User.ROLE_ADMIN)
+	public boolean deleteNews(String ID) {
+	    return newsService.deleteNews(Integer.parseInt(ID));
+	}
+
 	@Post("/api/v2/users/addnews/{text}")
 	@Consumes(MediaType.TEXT_PLAIN)
 	@Secured(SecurityRule.IS_ANONYMOUS)
 	//TODO: only allow admin to add/delete news
 	//@Secured(User.ROLE_ADMIN)
-	//public boolean addNews( @Parameter("text") String  text) {
 	public boolean addNews(String text) {
-	    log.info("addNews with text="+text);
+	    // log.info("addNews with text="+text);
 		return newsService.addNews(text);
 	}
+
 	@Get("/api/v2/users/news")
 	@Secured(SecurityRule.IS_ANONYMOUS)
 	//@Secured(User.ROLE_ADMIN)
 	public String getNews() {
-	    log.info("getNews");
+	    // log.info("getNews");
 		return newsService.getNews();
 	}
 	@Get("/api/v2/users/latestnews")
