0e763c60968562f703981ab690c70c6d71500622
andrei-barysenka <andrei.barysenka@gmail.com>
2026-03-07 16:53:24 +0100

    add missing import

--- src/main/java/cloudgene/mapred/server/controller/NewsController.java
+++ src/main/java/cloudgene/mapred/server/controller/NewsController.java
@@ -12,6 +12,7 @@ import io.micronaut.security.annotation.Secured;
 import jakarta.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import cloudgene.mapred.core.User;
 
 @Controller
 public class NewsController {
