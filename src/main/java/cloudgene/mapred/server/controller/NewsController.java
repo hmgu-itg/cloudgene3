e87637113bc893c4626e1db3e03744a01870d736
andrei-barysenka <andrei.barysenka@gmail.com>
2026-03-07 17:12:44 +0100

    add missing import

--- src/main/java/cloudgene/mapred/server/controller/NewsController.java
+++ src/main/java/cloudgene/mapred/server/controller/NewsController.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import cloudgene.mapred.core.User;
 import io.micronaut.security.authentication.Authentication;
+import cloudgene.mapred.server.auth.AuthenticationService;
 
 @Controller
 public class NewsController {
