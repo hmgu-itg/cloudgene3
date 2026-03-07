117409a172c7d291faf14d15394199bf42127044
andrei-barysenka <andrei.barysenka@gmail.com>
2026-03-07 14:10:45 +0100

    add Post

--- src/main/java/cloudgene/mapred/server/controller/NewsController.java
+++ src/main/java/cloudgene/mapred/server/controller/NewsController.java
@@ -5,6 +5,7 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.context.annotation.Parameter;
 import io.micronaut.http.annotation.Consumes;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
 import io.micronaut.security.rules.SecurityRule;
 import io.micronaut.security.annotation.Secured;
 import jakarta.inject.Inject;
